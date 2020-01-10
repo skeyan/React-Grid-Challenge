571f14f41caa71368517709af446f15940179310
davidy9000 <davidy.9000@gmail.com>
2020-01-10 11:36:36 -0500

    Implemented the TableRow and TableCell components but still have to work on Table and color.

---
+++ src/components/TableRow.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import TableCell from './TableCell';
+import './TableRow.css';
+
+class TableRow extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            numOfCells: props.numOfCells,
+            cells: []
+        }
+        this.populateArray()
+    }
+
+    populateArray = () => {
+        for(let i=0; i < this.state.numOfCells; i++){
+            this.state.cells[i] = <TableCell />;
+        }
+    }
+    
+    render() {
+        // console.log(this.state.numOfCells);
+        
+        console.log(this.state.cells);
+
+        return <tr>{this.state.cells}</tr>
+
+        
+    }
+}
+
+export default TableRow;
